d65e65d7c856f8dd259163dc34737cec4dd75ddc
veronnah <yoo.veroon@gmail.com>
2021-12-01 21:52:27 +0100

    first section done, swiper added

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// const swiper = new Swiper('.mini__slider', {
+//     loop: true,
+
+//     pagination: {
+//       el: '.swiper-pagination',
+//       clickable: true,
+//     },
+
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+
+//     scrollbar: {
+//       el: '.swiper-scrollbar',
+
+//     },
+//   });
